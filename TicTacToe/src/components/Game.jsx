520bdd4b682bb7a2915d04662f1f9078cb8cb697
N-Sikder <velocityx72@gmail.com>
2024-04-08 01:18:02 +0600

    Added Styles for winning Squares.

--- TicTacToe/src/components/Game.jsx
+++ TicTacToe/src/components/Game.jsx
@@ -10,10 +10,10 @@ import {
 } from "@chakra-ui/react";
 
 //The square component
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, isWinningSquare }) {
   return (
     <Button
-      bg="blackAlpha.300"
+      bg={isWinningSquare ? "green.200" : "blackAlpha.300"}
       size="lg"
       w="75px"
       h="75px"
@@ -34,8 +34,8 @@ export default function Game() {
   const winner = calculateWinner(squares);
   let status;
   //To display the winner or the display whose move is next
-  if (winner) {
-    status = `Winner is ${winner}`;
+  if (winner !== null) {
+    status = `Winner is ${winner.winnerName}`;
   } else if (squares.every((square) => square !== null)) {
     status = "It's a Draw!";
   } else if (squares.every((square) => square == null)) {
@@ -46,10 +46,10 @@ export default function Game() {
 
   function handleClick(i) {
     if (squares[i] !== null || calculateWinner(squares)) {
+      console.log(calculateWinner(squares));
       setShowAlert(true); // Show the alert
       return; // Exit the function early
     }
-
     const copySquares = [...squares];
 
     if (isNext) {
@@ -72,20 +72,57 @@ export default function Game() {
         </Alert>
       )}
       <Box>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+          isWinningSquare={winner && winner.winningSquare.includes(0)}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+          isWinningSquare={winner && winner.winningSquare.includes(1)}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+          isWinningSquare={winner && winner.winningSquare.includes(2)}
+        />
       </Box>
       <Box>
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+          isWinningSquare={winner && winner.winningSquare.includes(3)}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+          isWinningSquare={winner && winner.winningSquare.includes(4)}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+          isWinningSquare={winner && winner.winningSquare.includes(5)}
+        />
       </Box>
       <Box>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+          isWinningSquare={winner && winner.winningSquare.includes(6)}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+          isWinningSquare={winner && winner.winningSquare.includes(7)}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+          isWinningSquare={winner && winner.winningSquare.includes(8)}
+        />
       </Box>
+
       <Text fontSize="2xl">{status}</Text>
     </>
   );
@@ -103,11 +140,20 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+  console.log("squares", squares);
   //We need to loop through our main array and enter into each sub-array and check if our played game's array is a match with any of the above winning combinations.
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] === squares[b] && squares[b] === squares[c]) {
-      return squares[a];
+    if (
+      squares[a] !== null &&
+      squares[a] === squares[b] &&
+      squares[b] === squares[c]
+    ) {
+      const winnerInfo = {
+        winnerName: squares[a],
+        winningSquare: [a, b, c],
+      };
+      return winnerInfo;
     }
   }
   return null;
